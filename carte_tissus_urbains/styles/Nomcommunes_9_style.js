06ed5b8f5b5be2cc84551f41be61fcd5a6cdb63a
khadijakhalli <khallikhadija.k4@gmail.com>
2025-04-14 01:20:05 +0200

    Add files via upload

---
+++ carte_tissus_urbains/styles/Nomcommunes_9_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_Nomcommunes_9 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; var value = feature.get("");
+    var labelFont = "10.4px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 0.8;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("NOM") !== null) {
+        labelText = String(feature.get("NOM"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(255,255,255,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
